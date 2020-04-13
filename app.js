5ca2a24f2b14c1de0e6bf9c48dc1b3e9ba4aaaa6
parkoon <devparkoon@gmail.com>
2020-04-13 18:38:15 +0900

    Handling post request

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
+app.use(express.json());
+
 const PORT = process.env.PORT || 4000;
 
 const tours = JSON.parse(
-  fs.readFileSync(path.join(__dirname, 'dev-data/data/tours.json'), 'utf-8')
+  fs.readFileSync(
+    path.join(__dirname, 'dev-data/data/tours-simple.json'),
+    'utf-8'
+  )
 );
 
 app.get('/api/v1/tours', (req, res) => {
@@ -15,6 +20,21 @@ app.get('/api/v1/tours', (req, res) => {
     results: tours.length,
     data: { tours },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const nextId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: nextId }, req.body);
+  tours.push(newTour);
+
+  fs.writeFile(
+    path.join(__dirname, 'dev-data/data/tours-simple.json'),
+    JSON.stringify(tours),
+    (err) => {
+      if (err) return res.status(500).json({ status: 'failure', error: err });
+      res.status(201).json({ status: 'success', data: { tour: newTour } });
+    }
+  );
 });
 
 app.listen(PORT, () => console.log(`Server is runngin on ${PORT}`));
