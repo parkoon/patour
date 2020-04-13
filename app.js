6b955d5d2ba4c9370e20899fb5a220799636c752
parkoon <devparkoon@gmail.com>
2020-04-13 18:09:34 +0900

    Handling get request

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.status(200).json({
-    success: true,
-    foo: 'bar',
+const tours = JSON.parse(
+  fs.readFileSync(path.join(__dirname, 'dev-data/data/tours.json'), 'utf-8')
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.json({
+    status: 'success',
+    results: tours.length,
+    data: { tours },
   });
 });
 
