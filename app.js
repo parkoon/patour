0302922352edce3845986fd7a2047216ff5390d9
parkoon <devparkoon@gmail.com>
2020-04-14 15:02:23 +0900

    serving static files

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 const app = express();
 
 const tourRouter = require('./routes/tourRoute');
@@ -7,6 +8,7 @@ const userRouter = require('./routes/userRoute');
 
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
