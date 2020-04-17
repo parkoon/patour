59c0d2a49df672c444a13d8c3701a0be771f1807
parkoon <devparkoon@gmail.com>
2020-04-17 10:41:25 +0900

    connect database

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config();
+
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB is connected'))
+  .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server is runngin on ${PORT}`));
