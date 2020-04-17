8ab94e0c338a30b1db3ce40c849f39eb0f760322
parkoon <devparkoon@gmail.com>
2020-04-17 12:55:21 +0900

    mvc pattern
    thin controller / fat model

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,23 +1,23 @@
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 
-const tours = JSON.parse(
-  fs.readFileSync(
-    path.join(__dirname, '../dev-data/data/tours-simple.json'),
-    'utf-8'
-  )
-);
+// const tours = JSON.parse(
+//   fs.readFileSync(
+//     path.join(__dirname, '../dev-data/data/tours-simple.json'),
+//     'utf-8'
+//   )
+// );
 
-exports.checkId = (req, res, next, val) => {
-  const tour = tours.find((t) => t.id === val * 1);
-  if (!tour) {
-    return res.status(404).json({
-      status: 'failure',
-      error: 'tour not found',
-    });
-  }
-  next();
-};
+// exports.checkId = (req, res, next, val) => {
+//   const tour = tours.find((t) => t.id === val * 1);
+//   if (!tour) {
+//     return res.status(404).json({
+//       status: 'failure',
+//       error: 'tour not found',
+//     });
+//   }
+//   next();
+// };
 
 exports.checkBody = (req, res, next) => {
   const required = ['name', 'price'];
@@ -36,37 +36,36 @@ exports.checkBody = (req, res, next) => {
 exports.getTours = (req, res) => {
   res.json({
     status: 'success',
-    results: tours.length,
-    data: { tours },
+    // results: tours.length,
+    // data: { tours },
   });
 };
 exports.getTour = (req, res) => {
-  const { id } = req.params;
-  const tour = tours.find((t) => t.id === id * 1);
+  // const { id } = req.params;
+  // const tour = tours.find((t) => t.id === id * 1);
   res.json({
     status: 'success',
     request_at: req.requestTime,
-    data: {
-      tour,
-    },
+    // data: {
+    //   tour,
+    // },
   });
 };
 exports.createTour = (req, res) => {
-  const nextId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({ id: nextId }, req.body);
-  tours.push(newTour);
-
-  fs.writeFile(
-    path.join(__dirname, '../dev-data/data/tours-simple.json'),
-    JSON.stringify(tours),
-    (err) => {
-      if (err) return res.status(500).json({ status: 'failure', error: err });
-      res.status(201).json({ status: 'success', data: { tour: newTour } });
-    }
-  );
+  // const nextId = tours[tours.length - 1].id + 1;
+  // const newTour = Object.assign({ id: nextId }, req.body);
+  // tours.push(newTour);
+  // fs.writeFile(
+  //   path.join(__dirname, '../dev-data/data/tours-simple.json'),
+  //   JSON.stringify(tours),
+  //   (err) => {
+  //     if (err) return res.status(500).json({ status: 'failure', error: err });
+  //     res.status(201).json({ status: 'success', data: { tour: newTour } });
+  //   }
+  // );
 };
 exports.udpateTour = (req, res) => {
-  const { id } = req.params;
+  // const { id } = req.params;
   res.json({
     status: 'success',
     tour: 'updated tour....',
--- routes/tourRoute.js
+++ routes/tourRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const tourController = require('../controllers/tourController');
+
 const router = express.Router();
 
-router.param('id', tourController.checkId);
+// router.param('id', tourController.checkId);
 
 router
   .route('/:id')
