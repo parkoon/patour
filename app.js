99dc7a162d4e3754e9d3f2b561127b69ab319f80
parkoon <devparkoon@gmail.com>
2020-04-19 14:23:26 +0900

    Handling unhandled routes
     ** 마지막에 위치해야 한다!

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const userRouter = require('./routes/userRoute');
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
+// 1) MIDDLEWARES
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -19,7 +19,15 @@ app.use((req, res, next) => {
   next();
 });
 
+// 2) ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'failure',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
