3c769a0bc1f29342a5219673c7897f3a3a91bebb
parkoon <devparkoon@gmail.com>
2020-04-20 18:27:24 +0900

    sending JWT via cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,10 +14,20 @@ const signToken = (id) => {
 
 const createAndSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true; // https only
+
+  res.cookie('jwt', token, cookieOptions);
+
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
-    token,
     data: {
       user,
     },
