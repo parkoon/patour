9727ac4fb9daa7a5a12849ce7745461186121ce2
parkoon <devparkoon@gmail.com>
2020-04-20 17:20:12 +0900

    update current user
    - req.body 를 전부 업데이트 하는 것이 아닌, 필요한 필드만 filtering 한다.
    - findByIdAndUpdate 메소드를 이용하되, {new: true, runValidators: true}

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,38 @@
+const User = require('../models/userModel');
+const catchAsync = require('../helpers/catchAsync');
+const AppError = require('../helpers/appError');
+
+const filterObj = (target, ...allowedFields) => {
+  const obj = {};
+  Object.keys(target).forEach((key) => {
+    if (allowedFields.includes(key)) obj[key] = target[key];
+  });
+  return obj;
+};
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) 패스워드 수정하러 들어온 사람 쫒아냄
+  if (req.body.password || req.body.passwordConfirm) return next(AppError);
+
+  // 2) 유저를 찾음 & 업데이트
+  const filteredObj = filterObj(req.body, 'name', 'role');
+
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    { ...filteredObj },
+    { new: true, runValidators: true }
+  );
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+
+  //
+});
+
 exports.getUsers = (req, res) => {
   res.status(500).json({
     status: 'error',
