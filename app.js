c51b52b1d3d14c43f613195b16480110f4bd5ccd
parkoon <devparkoon@gmail.com>
2020-04-20 18:50:38 +0900

    prevent from xss attact with data sanitizatoin🛡

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const morgan = require('morgan');
 const path = require('path');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./helpers/appError');
 const globalErrorHander = require('./controllers/errorController');
@@ -31,6 +33,12 @@ app.use('/api', limiter);
 // Body Parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 // app.use(express.urlencoded({ extended: true }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
