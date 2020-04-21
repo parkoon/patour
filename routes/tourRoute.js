55a585f03c92f7e8143df9dd754216efc82691e2
parkoon <devparkoon@gmail.com>
2020-04-21 18:12:34 +0900

    update router auth👮‍♀️

--- routes/tourRoute.js
+++ routes/tourRoute.js
@@ -14,7 +14,22 @@ router
   .get(tourController.aliasTopTours, tourController.getAllTours);
 
 router.route('/stats').get(tourController.getTourStats);
-router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+router
+  .route('/monthly-plan/:year')
+  .get(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide', 'guide'),
+    tourController.getMonthlyPlan
+  );
+
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.createTour
+  );
 
 router
   .route('/:id')
@@ -24,12 +39,10 @@ router
     tourController.deleteTour
   )
   .get(tourController.getTour)
-  .patch(tourController.updateTour);
-
-router
-  .route('/')
-  .get(tourController.getAllTours)
-  // .post(tourController.checkBody, tourController.createTour);
-  .post(tourController.createTour);
+  .patch(
+    authController.protect,
+    authController.restrictTo('admin', 'lead-guide'),
+    tourController.updateTour
+  );
 
 module.exports = router;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,31 +6,28 @@ const router = express.Router();
 
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-router.get(
-  '/me',
-  authController.protect,
-  userController.getMe,
-  userController.getUser
-);
-
-router.patch('/resetPassword/:token', authController.resetPassword);
 router.post('/forgotPassword', authController.forgotPassword);
-router.patch(
-  '/updatePassword',
-  authController.protect,
-  authController.updatePassword
-);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
+router.patch('/resetPassword/:token', authController.resetPassword);
+
+// Protect all routes after this middleware
+router.use(authController.protect);
+
+router.patch('/updateMyPassword', authController.updatePassword);
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
+
+router.use(authController.restrictTo('admin'));
+
+router
+  .route('/')
+  .get(userController.getAllUsers)
+  .post(userController.createUser);
 
 router
   .route('/:id')
   .get(userController.getUser)
-  .delete(userController.deleteUser)
-  .patch(userController.updateUser);
-router
-  .route('/')
-  .post(userController.createUser)
-  .get(userController.getAllUsers);
+  .patch(userController.updateUser)
+  .delete(userController.deleteUser);
 
 module.exports = router;
