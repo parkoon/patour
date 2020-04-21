3db57cd6148588680a4a6e51a7064ff5a4c9ad82
parkoon <devparkoon@gmail.com>
2020-04-21 16:26:21 +0900

    populating review

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -33,6 +33,17 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+reviewSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: 'tour',
+    select: 'name',
+  }).populate({
+    path: 'user',
+    select: 'name photo',
+  });
+  next();
+});
+
 const reviewModel = mongoose.model('Review', reviewSchema);
 
 module.exports = reviewModel;
