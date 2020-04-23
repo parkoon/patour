7e68680755348b722474138fb03d5710125f4063
parkoon <devparkoon@gmail.com>
2020-04-23 14:02:53 +0900

    udpate alert

--- public/js/alert.js
+++ public/js/alert.js
@@ -1,6 +1,8 @@
 export const showAlert = (type, message) => {
+  hideAlert();
   const markup = `<div class="alert alert--${type}">${message}</div>`;
   document.querySelector('body').insertAdjacentHTML('afterbegin', markup);
+  window.setTimeout(hideAlert, 1000);
 };
 
 export const hideAlert = () => {
